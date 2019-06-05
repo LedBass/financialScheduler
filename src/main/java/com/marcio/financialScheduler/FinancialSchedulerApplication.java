2e2963fd05497edcbac3df5ed65c055ba0fed601
LedBass <marcioalvesdasilva60@gmail.com>
2019-06-04 21:26:38 -0300

    First commit
    This initial commit contains only the very start of the project, the code will be added in new commits and/or pull requests from created branches

---
+++ src/main/java/com/marcio/financialScheduler/FinancialSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.financialScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcio/financialScheduler/FinancialSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.financialScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
